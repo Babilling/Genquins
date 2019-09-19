419511310edda6c523629d2c78d7b7bb8e203d5f
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-09-20 00:33:34 +0200

    The webpage is now working correctly

--- src/main/java/github/kaysoro/Genquins/controller/WebController.java
+++ src/main/java/github/kaysoro/Genquins/controller/WebController.java
@@ -1,13 +1,11 @@
 package github.kaysoro.Genquins.controller;
 
 import github.kaysoro.Genquins.service.ChallongeClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebController {
 
     private ChallongeClient challongeClient;
