b21942e8c29ec29d04f2c4a17b8749b481a6d9b2
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2019-09-20 18:27:31 +0200

    lul

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-    $("#search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#match tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
     $('#matches').DataTable();
 });
