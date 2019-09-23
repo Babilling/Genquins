86ad74b62ac954dc02fd365fe165ed09d95d2622
Maxime Genaudet <maxime.genaudet@soprabanking.com>
2019-09-23 13:58:47 +0200

    marche pas omegalul

--- src/main/java/github/kaysoro/Genquins/controller/WebController.java
+++ src/main/java/github/kaysoro/Genquins/controller/WebController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Controller
@@ -26,13 +27,14 @@ public class WebController {
 
     @GetMapping({ "/", "/index" })
     public String index(Model model) {
-        model.addAttribute("matchs", matchService.getModelMatches());
+        model.addAttribute("matchs", matchService.getModelMatches()
+                .collectMap(match-> match.getId(), Function.identity()));
         model.addAttribute("matchToSave", Match.builder().build());
         return "index";
     }
 
     @PostMapping("/save")
     public String save(Model model, Match matchToSave) {
-        return index(model);
+        return "index";
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,11 +3,8 @@ $(document).ready(function(){
 });
 $('#scoresModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
-  var par1 = button.data('participant1-name') // Extract info from data-* attributes
-  var par2 = button.data('participant2-name')
+  var matchId = button.data('matchId') // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-  modal.find('#participant1Name').text(par1)
-  modal.find('#participant2Name').text(par2)
-})
+});
