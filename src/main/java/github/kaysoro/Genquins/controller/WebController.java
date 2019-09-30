e136f855deb5f59722e1af591863cc35ccb0ce38
Steven Pescheteau <steven.pescheteau@gmail.com>
2019-09-30 18:00:48 +0200

    Clean code.

--- src/main/java/github/kaysoro/Genquins/controller/WebController.java
+++ src/main/java/github/kaysoro/Genquins/controller/WebController.java
@@ -1,6 +1,5 @@
 package github.kaysoro.Genquins.controller;
 
-import github.kaysoro.Genquins.mapper.MatchMapper;
 import github.kaysoro.Genquins.model.Match;
 import github.kaysoro.Genquins.service.ChallongeClient;
 import github.kaysoro.Genquins.service.MatchService;
@@ -9,12 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Controller
 public class WebController {
--- src/main/java/github/kaysoro/Genquins/service/MatchService.java
+++ src/main/java/github/kaysoro/Genquins/service/MatchService.java
@@ -1,10 +1,8 @@
 package github.kaysoro.Genquins.service;
 
-import github.kaysoro.Genquins.controller.ChallongeController;
 import github.kaysoro.Genquins.mapper.MatchMapper;
 import github.kaysoro.Genquins.model.Match;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import github.kaysoro.Genquins.payload.Participant;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -13,15 +11,13 @@ public class MatchService {
 
     private ChallongeClient challongeClient;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ChallongeController.class);
-
     public MatchService(ChallongeClient challongeClient){
         this.challongeClient = challongeClient;
     }
 
     public Flux<Match> getModelMatches() {
         return challongeClient.getAllParticipantsForTournament()
-                .collectMap(p -> p.getId(), participant -> participant)
+                .collectMap(Participant::getId, participant -> participant)
                 .flatMapMany(participants -> challongeClient.getAllMatchesForTournament()
                         .map(match -> MatchMapper.map(match, participants)));
         }
